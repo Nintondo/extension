2e15c7c8b56573ca81b749678a7fc5d034311a62
Kiel Ed <kieledssh@gmail.com>
2024-01-14 03:24:11 +0300

    updated inscription interface

--- src/shared/interfaces/inscriptions.ts
+++ src/shared/interfaces/inscriptions.ts
@@ -1,54 +1,41 @@
 import { ApiUTXO } from "./api";
 
-export interface Inscription extends ApiUTXO {
-  inscription_id?: string;
-  address?: string;
-  outputValue?: number;
-  preview?: string;
-  content?: string;
-  contentLength?: number;
-  contentType?: "image/png";
-  timeStamp?: string;
-  genesisHeight?: number;
-  genesisFee?: number;
-  genesisTransaction?: string;
-  location?: string;
-  output?: string;
-  offset?: number;
-  isOrd: true;
-}
-
-export const TestInscription: Inscription = {
-  inscription_id:
-    "a77d282813922a140cee709d5afe644d8836d430fc1ac2c824fbbe282d661944i0",
-  address: "BPAiWMThT2ZwhkyoG27poU1HVNmKsRk7K4",
-  outputValue: 100000,
-  preview:
-    "https://bellinals.nintondo.io/preview/a77d282813922a140cee709d5afe644d8836d430fc1ac2c824fbbe282d661944i0",
-  content:
-    "https://bellinals.nintondo.io/content/a77d282813922a140cee709d5afe644d8836d430fc1ac2c824fbbe282d661944i0",
-  contentLength: 694,
-  contentType: "image/png",
-  timeStamp: "2023-12-25 22:20:50 UTC",
-  genesisHeight: 22490,
-  genesisFee: 34200,
-  genesisTransaction:
-    "a33eddbfb700301038f2e25d34cbe2b55c1602d4138967f37759c9227d6d0b29",
-  location:
-    "a33eddbfb700301038f2e25d34cbe2b55c1602d4138967f37759c9227d6d0b29:0:0",
-  output: "a33eddbfb700301038f2e25d34cbe2b55c1602d4138967f37759c9227d6d0b29:0",
-  offset: 0,
-  txid: "",
-  vout: 0,
-  status: undefined,
-  value: 0,
-  isOrd: true,
-};
-
-export interface ApiOrdUtxo extends ApiUTXO {
+interface ApiOrdUTXO extends ApiUTXO {
   inscription_id: string;
   content_type: string;
   content_length: number;
   outpoint: string;
   genesis: string;
+  owner: string;
 }
+
+export interface Inscription extends ApiOrdUTXO {
+  preview: string;
+  content: string;
+  offset: number;
+}
+
+export const TestInscription: Inscription = {
+  inscription_id:
+    "a77d282813922a140cee709d5afe644d8836d430fc1ac2c824fbbe282d661944i0",
+  owner: "BPAiWMThT2ZwhkyoG27poU1HVNmKsRk7K4",
+  value: 100000,
+  preview:
+    "https://bellinals.nintondo.io/preview/a77d282813922a140cee709d5afe644d8836d430fc1ac2c824fbbe282d661944i0",
+  content:
+    "https://bellinals.nintondo.io/content/a77d282813922a140cee709d5afe644d8836d430fc1ac2c824fbbe282d661944i0",
+  content_length: 694,
+  content_type: "image/png",
+  genesis: "a33eddbfb700301038f2e25d34cbe2b55c1602d4138967f37759c9227d6d0b29",
+  outpoint:
+    "a33eddbfb700301038f2e25d34cbe2b55c1602d4138967f37759c9227d6d0b29:0:0",
+  offset: 0,
+  txid: "",
+  vout: 0,
+  status: {
+    block_height: 22490,
+    block_hash: "",
+    block_time: 1724142142,
+    confirmed: true,
+  },
+};
--- src/ui/pages/main/inscription-details/components.tsx
+++ src/ui/pages/main/inscription-details/components.tsx
@@ -8,35 +8,72 @@ import { useLocation, useNavigate } from "react-router-dom";
 import Loading from "react-loading";
 import Iframe from "@/ui/components/iframe";
 
-interface InscriptionItem {
-  name: string;
-  value: any;
-  link: boolean;
+type PathOf<T> = T extends object
+  ? {
+      [K in keyof T & (string | number)]: K extends string | number
+        ? `${K}` | (T[K] extends object ? `${K}.${PathOf<T[K]>}` : never)
+        : never;
+    }[keyof T & (string | number)]
+  : "";
+
+interface InscField<T, K extends PathOf<T> = PathOf<T>> {
+  key?: K;
+  link?: boolean;
 }
+
+const fields: InscField<Inscription>[] = [
+  {
+    key: "content_length",
+  },
+  {
+    key: "status.block_height",
+  },
+  {
+    key: "content_type",
+  },
+  {
+    key: "genesis",
+  },
+  {
+    key: "preview",
+    link: true,
+  },
+  {
+    key: "content",
+    link: true,
+  },
+  {
+    key: "offset",
+  },
+  {
+    key: "owner",
+  },
+  {
+    key: "value",
+  },
+  {
+    key: "outpoint",
+  },
+  {
+    key: "inscription_id",
+  },
+  {
+    key: "status.block_time",
+  },
+];
 
 const InscriptionDetails = () => {
   const currentAccount = useGetCurrentAccount();
   const navigate = useNavigate();
   const location = useLocation();
-  const [inscriptionData, setInscirptionData] = useState<InscriptionItem[]>([]);
   const [inscription, setInscription] = useState<Inscription | undefined>(
     undefined
   );
 
   useEffect(() => {
-    if (!location.state) navigate(-1);
-    const stateInscription = location.state as Inscription;
-    const toReturn: InscriptionItem[] = [];
-    for (const key in stateInscription) {
-      toReturn.push({
-        name: t(`components.inscription_card.${key}`),
-        value: stateInscription[key],
-        link: key === "content" || key === "preview",
-      });
-    }
-    setInscription(stateInscription);
-    setInscirptionData(toReturn);
-  }, [location, setInscirptionData, navigate]);
+    if (!location.state) return navigate(-1);
+    setInscription(location.state);
+  }, [location, navigate]);
 
   const openContent = async (link: string) => {
     await browserTabsCreate({
@@ -49,6 +86,14 @@ const InscriptionDetails = () => {
     navigate("/pages/create-send", { state: inscription });
   };
 
+  const getValue = <T,>(key: string) => {
+    let current = inscription;
+    for (const i in key.split(".")) {
+      current = current[i];
+    }
+    return current as T;
+  };
+
   if (inscription === undefined) return <Loading />;
 
   return (
@@ -56,25 +101,25 @@ const InscriptionDetails = () => {
       <div className="flex w-full justify-center">
         <Iframe preview={inscription.preview} size="big" />
       </div>
-      {inscription.address === currentAccount.address ? (
+      {inscription.owner === currentAccount.address ? (
         <button onClick={send} className="btn primary mx-4 mb-4 md:m-6 md:mb-3">
           {t("components.layout.send")}
         </button>
       ) : undefined}
-      {inscriptionData.map((f, i) => (
+      {fields.map((f, i) => (
         <div key={i}>
-          <label>{f.name}</label>
+          <label>{t(`components.inscription_details.${f.key}`)}</label>
           {f.link ? (
             <p
               onClick={async () => {
-                await openContent(f.value);
+                await openContent(getValue<string>(f.key));
               }}
               className="text-orange-400"
             >
               link
             </p>
           ) : (
-            <p>{f.value}</p>
+            <p>{inscription[f.key]}</p>
           )}
         </div>
       ))}
