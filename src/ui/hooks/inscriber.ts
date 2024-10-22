11ca4e03b627ae382f9f279097da074b7a574fcd
Kiel Ed <kieledssh@gmail.com>
2024-10-22 10:58:28 +0300

    fix inscribe tokens

--- src/ui/hooks/inscriber.ts
+++ src/ui/hooks/inscriber.ts
@@ -3,7 +3,7 @@ import { inscribe } from "bells-inscriber";
 import { useControllersState } from "../states/controllerState";
 import toast from "react-hot-toast";
 import { t } from "i18next";
-import { ss } from "../utils";
+import { isValidTXID, ss } from "../utils";
 import { useAppState } from "../states/appState";
 import { useGetCurrentAccount } from "../states/walletState";
 import { ApiUTXO } from "bells-inscriber/lib/types";
@@ -30,39 +30,28 @@ export const useInscribeTransferToken = () => {
   return async (data: ITransferToken, feeRate: number) => {
     if (!currentAccount || !currentAccount.address) return;
 
-    let txs: string[] | undefined;
-
-    try {
-      txs = await inscribe({
-        toAddress: currentAccount.address,
-        fromAddress: currentAccount.address,
-        data: Buffer.from(JSON.stringify(data)),
-        feeRate,
-        contentType: "application/json; charset=utf-8",
-        publicKey: Buffer.from(
-          await keyringController.exportPublicKey(currentAccount.address),
-          "hex"
-        ),
-        signPsbt: keyringController.signPsbtBase64,
-        getUtxos,
-        network,
-      });
-    } catch (e) {
-      if (e instanceof Error) toast.error(e.message);
-      throw e;
-    }
+    const txs = await inscribe({
+      toAddress: currentAccount.address,
+      fromAddress: currentAccount.address,
+      data: Buffer.from(JSON.stringify(data)),
+      feeRate,
+      contentType: "application/json; charset=utf-8",
+      publicKey: Buffer.from(
+        await keyringController.exportPublicKey(currentAccount.address),
+        "hex"
+      ),
+      signPsbt: keyringController.signPsbtBase64,
+      getUtxos,
+      network,
+    });
 
     const txIds: string[] = [];
-    try {
-      for (const i of txs) {
-        txIds.push((await apiController.pushTx(i))?.txid ?? "");
-      }
-    } catch (e) {
-      if (e instanceof Error) {
-        toast.error(e.message);
-      }
+
+    for (const i of txs) {
+      txIds.push((await apiController.pushTx(i))?.txid ?? "");
     }
-    if (!txIds.filter((f) => f.length !== 64 || f.includes("RPC error")).length)
+
+    if (txIds.every(isValidTXID))
       toast.success(t("inscriptions.transfer_inscribed"));
     else toast.error(t("inscriptions.failed_inscribe_transfer"));
   };
